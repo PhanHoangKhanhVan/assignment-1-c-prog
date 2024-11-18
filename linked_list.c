dad369148e1f7651b6cb2f74c1c006c6c3e610f5
PhanHoangKhanhVan <10423181@student.vgu.edu.vn>
2024-11-18 21:31:24 +0700

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -6,23 +6,67 @@
 
 void insertNode(struct Node** p_head, struct Node* position, int data) 
 {
-    // your implementation
+     struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = data;
+
+    if (*p_head == NULL || position == *p_head) { // Thêm vào đầu
+        new_node->next = *p_head;
+        *p_head = new_node;
+    } else { // Tìm vị trí chèn
+        struct Node* temp = *p_head;
+        while (temp->next != NULL && temp->next != position) {
+            temp = temp->next;
+        }
+        new_node->next = temp->next;
+        temp->next = new_node;
+    }
 }
 
 void pushBack(struct Node** p_head, int data)
 {
-    // your implementation
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if (*p_head == NULL) { // Danh sách rỗng
+        *p_head = new_node;
+    } else { // Duyệt đến cuối danh sách
+        struct Node* temp = *p_head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
 }
 
 void deleteNode(struct Node** p_head, struct Node* position) 
 {
-    // your implementation
+   if (*p_head == NULL || position == NULL) return;
+
+    if (*p_head == position) { // Xóa node đầu
+        struct Node* temp = *p_head;
+        *p_head = (*p_head)->next;
+        free(temp);
+    } else { // Duyệt tìm node trước node cần xóa
+        struct Node* temp = *p_head;
+        while (temp->next != NULL && temp->next != position) {
+            temp = temp->next;
+        }
+        if (temp->next == position) {
+            temp->next = position->next;
+            free(position);
+        }
+    }
 }
 
 int sizeOfList(struct Node* head) 
 {
     int count = 0;
-    // your implementation
+    struct Node* temp = head;
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
 
     return count;
 }
@@ -51,6 +95,7 @@ void displayListToString(struct Node* head, char* output) {
         sprintf(temp_str, "%d -> ", temp->data);
         strcat(output, temp_str);
         temp = temp->next;
+        
     }
     // concatenate NULL to the output string    
     sprintf(temp_str, "NULL");
